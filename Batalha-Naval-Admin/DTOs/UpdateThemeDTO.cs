0f1f363580bb186ff04f54836707986f243d54d9
nycolas023 <nycolas023@hotmail.com>
2025-06-17 02:55:55 -0300

    Correções de bugs

---
+++ Batalha-Naval-Admin/DTOs/UpdateThemeDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace Batalha_Naval_Admin.DTOs
+{
+    public class UpdateThemeDTO
+    {
+        public int ThemeId { get; set; }
+        public string? ThemeName { get; set; }
+        public string? ThemeImage { get; set; }
+        public string? ThemePreview { get; set; }
+        public int ThemePrice { get; set; }
+    }
+}
